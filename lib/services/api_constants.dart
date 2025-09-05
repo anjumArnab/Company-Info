ab1036fde6c066ae96fa29ed0bda0297971b42b5
unknown <anjumarnab050@gmail.com>
2025-09-05 22:44:15 +0600

    getter for GET_INFO

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,6 +1,7 @@
 class ApiConstants {
   static const String BASE_URL = "https://retoolapi.dev/0PWEu7/";
-  static const String GET_INFO = "data";
+  static const String ENDPOINT = "data";
+  static String get GET_INFO => BASE_URL + ENDPOINT;
 }
 
 enum Method { GET, POST, PUT, DELETE }
